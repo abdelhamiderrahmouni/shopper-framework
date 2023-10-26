fa3ae463e6e3055a7f23953499f5d5667a41a797
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-10-26 17:54:29 +0100

    updated brand.blade.php and application-logo.blade.php

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,2 @@
-<img {{ $attributes->merge(['class' => 'dark:hidden']) }} src="{{ asset('shopper/images/shopper-logo.svg') }}" alt="Laravel Shopper" />
-<img {{ $attributes->merge(['class' => 'hidden dark:inline']) }} src="{{ asset('shopper/images/shopper-logo-white.svg') }}" alt="Laravel Shopper" />
+<img {{ $attributes->merge(['class' => 'dark:hidden']) }} src="{{ asset('shopper/images/shopper-logo.svg') }}" alt="{{ config('app.name') }}" />
+<img {{ $attributes->merge(['class' => 'hidden dark:inline']) }} src="{{ asset('shopper/images/shopper-logo-white.svg') }}" alt="{{ config('app.name') }}" />
--- resources/views/components/brand.blade.php
+++ resources/views/components/brand.blade.php
@@ -1,5 +1,5 @@
 @if (filled($brand = config('shopper.system.brand')))
     <img {{ $attributes }} src="{{ asset($brand) }}" alt="{{ config('app.name') }}" />
 @else
-    <img {{ $attributes }} src="{{ asset('shopper/images/shopper-icon.svg') }}" alt="Laravel Shopper" />
+    <img {{ $attributes }} src="{{ asset('shopper/images/shopper-icon.svg') }}" alt="{{ config('app.name') }}" />
 @endif
