41df44478ff64634e9149fe0b761fab276e85b57
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-11-23 11:09:22 +0100

    fixed app_name display in footer.blade.php

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <p class="mt-8 font-medium text-sm text-center text-secondary-500 dark:text-secondary-400">
-    Copyright &copy; {{ date('Y') }} &mdash; {{ config('system.app_name') }}
+    Copyright &copy; {{ date('Y') }} &mdash; {{ config('shopper.system.app_name') }}
 </p>
