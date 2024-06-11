2aac22229348f4a53fa931f13b08da357e5d4517
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2024-06-11 14:16:40 +0100

    Added Attributes to variant.blade.php

--- resources/views/livewire/products/variant.blade.php
+++ resources/views/livewire/products/variant.blade.php
@@ -95,6 +95,23 @@
 
     <x-shopper::separator />
 
+    <div class="mt-6">
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="md:col-span-1">
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-lg font-semibold leading-6 text-secondary-900 dark:text-white">
+                        {{ __('shopper::pages/products.variants.variant_information') }}
+                    </h3>
+                </div>
+            </div>
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <livewire:shopper-products.form.attributes :product="$product" />
+            </div>
+        </div>
+    </div>
+
+    <x-shopper::separator />
+
     <div class="mt-10 sm:mt-0">
         <div class="md:grid md:grid-cols-3 md:gap-6">
             <div class="md:col-span-1">
